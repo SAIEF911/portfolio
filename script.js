e8cd68e5e5de65528952e34601e6213fb9c15205
SAIEF911 <saiefsara5@gmail.com>
2026-03-19 11:05:55 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const text = "Curious about Science & Tech";
+let index = 0;
 
+function type() {
+  if (index < text.length) {
+    document.querySelector(".typing").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, 50);
+  }
+}
+
+type();
